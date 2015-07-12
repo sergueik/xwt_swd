42160e1b285a6fe5d7fa2a7c40f84a84d15b0a31
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-07-12 14:08:15 -0400

    refactored in xamarin

--- UI/Program.cs
+++ UI/Program.cs
@@ -7,47 +7,47 @@ using UI;
 // https://github.com/mono/xwt/tree/master/TestApps/Samples
 namespace UI
 {
-    class Program
-    {
-        [STAThread]
-        static void Main(string[] args)
-        {
-            Application.Initialize(ToolkitType.Gtk);
-            Window MainWindow = new Window()
-            {
-                Title = "Xwt Demo Application",
-                Width = 500,
-                Height = 400
-            };
-            MainWindow.CloseRequested += (o, e) =>
-            {
-                Application.Exit();
-            };
-            Menu MainMenu = new Menu();
-            RichTextView TextView = new RichTextView();
-            MenuItem FileOpenMenuItem = new MenuItem("???????");
-            Menu FileMenu = new Menu();
-            FileOpenMenuItem.Clicked += (o, e) =>
-            {
-                OpenFileDialog Dialog = new OpenFileDialog("??????? ????");
-                if (Dialog.Run(MainWindow))
-                {
-                    TextView.LoadFile(Dialog.FileName, Xwt.Formats.TextFormat.Markdown);
-                }
-            };
-            MenuItem FileMenuItem = new MenuItem("????") { SubMenu = FileMenu };
-            FileMenu.Items.Add(FileOpenMenuItem);
-            MainMenu.Items.Add(FileMenuItem);
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Application.Initialize(ToolkitType.Gtk);
+			Window MainWindow = new Window()
+			{
+				Title = "Xwt Demo Application",
+				Width = 500,
+				Height = 400
+			};
+			MainWindow.CloseRequested += (o, e) =>
+			{
+				Application.Exit();
+			};
+			Menu MainMenu = new Menu();
+			RichTextView TextView = new RichTextView();
+			MenuItem FileOpenMenuItem = new MenuItem("???????");
+			Menu FileMenu = new Menu();
+			FileOpenMenuItem.Clicked += (o, e) =>
+			{
+				OpenFileDialog Dialog = new OpenFileDialog("??????? ????");
+				if (Dialog.Run(MainWindow))
+				{
+					TextView.LoadFile(Dialog.FileName, Xwt.Formats.TextFormat.Markdown);
+				}
+			};
+			MenuItem FileMenuItem = new MenuItem("????") { SubMenu = FileMenu };
+			FileMenu.Items.Add(FileOpenMenuItem);
+			MainMenu.Items.Add(FileMenuItem);
 
-            MainWindow.MainMenu = MainMenu;
-            var n = new NotebookSample();
+			MainWindow.MainMenu = MainMenu;
+			var n = new NotebookSample();
 
-            MainWindow.Content = n;
+			MainWindow.Content = n;
 
-            MainWindow.Show();
-            Application.Run();
-        }
-    }
+			MainWindow.Show();
+			Application.Run();
+		}
+	}
 
 
 }
--- UI/Properties/AssemblyInfo.cs
+++ UI/Properties/AssemblyInfo.cs
@@ -1,44 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SWD.Page.Recorder")]
-[assembly: AssemblyDescription("SWD Page Recorder")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SWD Page Recorder")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyTitle ("UI")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("sergueik")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38b41ca4-268b-11e5-b345-feff819cdc9f")]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(Build.Version)]
-[assembly: AssemblyFileVersion(Build.Version)]
+[assembly: AssemblyVersion ("1.0.*")]
 
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
 
-public class Build
-{
-    public const string Version = "1.0.0";
-    public const string WebDriverVersion = "v2.46";
-}
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
 
